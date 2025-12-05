6c10651efdee886bac1425678363220b940d8cda
José Miguel <jtutosgr@gmail.com>
2025-12-05 17:09:21 -0600

    fix netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    sourcemap: false,
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript; charset=utf-8',
+    },
+  },
 })
